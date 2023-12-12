4cc25160ecc41469a2203e328233fa281ff3a0ce
Martin Cortez <i20050008@monclova.tecnm.mx>
2023-12-12 15:03:53 -0600

    Initial commit

---
+++ DCOMVBCliente/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+
+Namespace U4_DCOMCliente
+    Module Program
+        Sub Main()
+            Try
+                Dim servidorDCOM As New ServidorDCOM()
+                Dim resultado As String = servidorDCOM.ObtenerSaludo("UsuarioDCOM")
+                Console.WriteLine(resultado)
+            Catch ex As Exception
+                Console.WriteLine($"Error: {ex.Message}")
+            End Try
+            Console.ReadLine()
+        End Sub
+    End Module
+End Namespace
+
